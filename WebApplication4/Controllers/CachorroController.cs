7b4bd804cb209beab1bcf936381ce245dd2887e5
MoniqueGabrielle02 <monique.costa2@etec.sp.gov.br>
2023-10-03 19:57:22 -0300

    Versão 03/10/2023, ultima atualização.

---
+++ WebApplication4/Controllers/CachorroController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class CachorroController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View(CachorroModel.Criarlista());
+        }
+
+        
+        public ActionResult Details(int id)
+        {
+            var cachorro = CachorroModel.Criarlista()[id];
+            return View(cachorro);
+        }
+
+      
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+    
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+              
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+     
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+               
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication4/Models/CachorroModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class CachorroModel
+    {
+        public string Raça { get; set; }
+        public string Sexo { get; set; }
+        public int Idade { get; set; }
+
+        public static List<CachorroModel> Criarlista()
+        {
+            var lista = new List<CachorroModel>();
+            
+
+            lista.Add(new CachorroModel() { Raça = "Pug", Sexo = "F", Idade = 2});
+            lista.Add(new CachorroModel() { Raça = "Poodle", Sexo = "F", Idade = 3});
+            lista.Add(new CachorroModel() { Raça = "Labrador", Sexo = "M", Idade = 1});
+            lista.Add(new CachorroModel() { Raça = "Pastor alemão", Sexo = "M", Idade = 4});
+            lista.Add(new CachorroModel() { Raça = "Shih tzu", Sexo = "F", Idade = 2});
+
+            return lista;
+        }
+    }
+}
